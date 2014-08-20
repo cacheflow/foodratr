e2613ffcfde0439160615de2bfbe5dce3d99502f
Lex Alexander <l.alexander10@gmail.com>
2014-08-19 23:13:40 -0700

    added raty which creates rating system.

---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,19 @@
+class UserController < ApplicationController
+  def create
+  end
+
+  def index
+  end
+
+  def destroy
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def show
+  end
+end
